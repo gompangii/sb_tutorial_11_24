89f3fb31cc3ff5d881eab94fb882682fda2ace61
Gompangii <gompangii@gmail.com>
2024-12-21 17:38:09 +0900

    여러가지 리턴타입2

--- src/main/java/com/gompnag/tutorial1/controller/HomeController.java
+++ src/main/java/com/gompnag/tutorial1/controller/HomeController.java
@@ -1,5 +1,8 @@
 package com.gompnag.tutorial1.controller;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,6 +17,7 @@ public class HomeController {
   public HomeController() {
     num = -1;
   }
+
   @GetMapping("/home/main")
   @ResponseBody
   public String showHome() {
@@ -41,8 +45,8 @@ public class HomeController {
 
   @GetMapping("/home/plus")
   @ResponseBody
-  public int showPlus(@RequestParam(defaultValue = "0") int a, int b){
-    return a+b;
+  public int showPlus(@RequestParam(defaultValue = "0") int a, int b) {
+    return a + b;
   }
 
   @GetMapping("/home/ReturnBoolean")
@@ -68,7 +72,7 @@ public class HomeController {
   @GetMapping("/home/ReturnIntList")
   @ResponseBody
   public List<Integer> showReturnIntList() {
-    List<Integer> list = new ArrayList<>(){{
+    List<Integer> list = new ArrayList<>() {{
       add(10);
       add(20);
       add(30);
@@ -102,6 +106,142 @@ public class HomeController {
     return map;
   }
 
+  @GetMapping("/home/ReturnArticle")
+  @ResponseBody
+  public Article showReturnArticle() {
+    Article article = new Article(1, "제목1", "내용1", "김철수", new ArrayList<>() {{
+      add(1);
+      add(2);
+      add(3);
+    }});
 
+    return article;
+  }
 
+  @GetMapping("/home/ReturnArticle2")
+  @ResponseBody
+  public Article2 showReturnArticle2() {
+    Article2 article = new Article2(1, "제목1", "내용1", "김철수", new ArrayList<>() {{
+      add(1);
+      add(2);
+      add(3);
+    }});
+
+    return article;
+  }
+
+  @GetMapping("/home/ReturnArticleMapList")
+  @ResponseBody
+  public List<Map<String, Object>> showReturnArticleMapList() {
+    Map<String, Object> articleMap1 = new LinkedHashMap<>() {{
+      put("id", 1);
+      put("subject", "제목1");
+      put("content", "내용1");
+      put("writerName", "김철수");
+      put("articleNo", new ArrayList<>() {{
+        add(1);
+        add(2);
+        add(3);
+      }});
+    }};
+
+    Map<String, Object> articleMap2 = new LinkedHashMap<>() {{
+      put("id", 2);
+      put("subject", "제목2");
+      put("content", "내용2");
+      put("writerName", "신짱구");
+      put("articleNo", new ArrayList<>() {{
+        add(4);
+        add(5);
+        add(6);
+      }});
+    }};
+
+    List<Map<String, Object>> list = new ArrayList<>();
+    list.add(articleMap1);
+    list.add(articleMap2);
+    return list;
+  }
+
+  @GetMapping("/home/ReturnArticleList")
+  @ResponseBody
+  public List<Article2> showReturnArticleList() {
+    Article2 article1 = new Article2(1, "제목1", "내용1", "김철수", new ArrayList<>() {{
+      add(1);
+      add(2);
+      add(3);
+    }});
+
+    Article2 article2 = new Article2(2, "제목2", "내용2", "신짱구", new ArrayList<>() {{
+      add(4);
+      add(5);
+      add(6);
+    }});
+
+    List<Article2> list = new ArrayList<>();
+    list.add(article1);
+    list.add(article2);
+
+    return list;
+  }
 }
+
+class Article {
+  private final int id;
+  private final String subject;
+  private final String content;
+  private final String writerName;
+  private final List<Integer> articleNo;
+
+  public int getId() {
+    return id;
+  }
+
+  public String getSubject() {
+    return subject;
+  }
+
+  public String getContent() {
+    return content;
+  }
+
+  public String getWriterName() {
+    return writerName;
+  }
+
+  public List<Integer> getArticleNo() {
+    return articleNo;
+  }
+
+
+  public Article(int id, String subject, String content, String writerName, List<Integer> articleNo) {
+    this.id = id;
+    this.subject = subject;
+    this.content = content;
+    this.writerName = writerName;
+    this.articleNo = articleNo;
+  }
+
+  @Override
+  public String toString() {
+    return "Article{" +
+        "id=" + id +
+        ", subject='" + subject + '\'' +
+        ", content='" + content + '\'' +
+        ", writerName='" + writerName + '\'' +
+        ", articleNo=" + articleNo +
+        '}';
+  }
+}
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+class Article2 {
+  int id;
+  String subject;
+  String content;
+  String writerName;
+  List<Integer> articleNo;
+}
+
