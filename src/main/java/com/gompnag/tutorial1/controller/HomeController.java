731613d336914ae4b2922587b14864622f498a45
Gompangii <gompangii@gmail.com>
2024-12-21 16:45:47 +0900

    여러가시 리턴 타입 테스트

--- src/main/java/com/gompnag/tutorial1/controller/HomeController.java
+++ src/main/java/com/gompnag/tutorial1/controller/HomeController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.*;
+
 @Controller
 public class HomeController {
   int num;
@@ -42,5 +44,64 @@ public class HomeController {
   public int showPlus(@RequestParam(defaultValue = "0") int a, int b){
     return a+b;
   }
+
+  @GetMapping("/home/ReturnBoolean")
+  @ResponseBody
+  public boolean showReturnBoolean() {
+    return true;
+  }
+
+  @GetMapping("/home/ReturnDouble")
+  @ResponseBody
+  public Double showReturnDouble() {
+    return Math.PI;
+  }
+
+  @GetMapping("/home/ReturnArray")
+  @ResponseBody
+  public int[] showReturnArray() {
+    int[] arr = new int[]{10, 20, 30};
+
+    return arr;
+  }
+
+  @GetMapping("/home/ReturnIntList")
+  @ResponseBody
+  public List<Integer> showReturnIntList() {
+    List<Integer> list = new ArrayList<>(){{
+      add(10);
+      add(20);
+      add(30);
+    }};
+
+    /*
+    List<Integer> list = new ArrayList<>();
+    list.add(10);
+    list.add(20);
+    list.add(30);
+     */
+
+    return list;
+  }
+
+  @GetMapping("/home/ReturnMap")
+  @ResponseBody
+  public Map<String, Object> showReturnMap() {
+    Map<String, Object> map = new LinkedHashMap<>() {{
+      put("id", 1);
+      put("subject", "제목1");
+      put("content", "내용1");
+      put("writerName", "김철수");
+      put("artticleNo", new ArrayList<>() {{
+        add(1);
+        add(2);
+        add(3);
+      }});
+    }};
+
+    return map;
+  }
+
+
 
 }
