eb0baec7d9b6fa9e9e5dab95f22537bb0339c5d8
Gompangii <gompangii@gmail.com>
2024-12-28 16:54:30 +0900

    49강, Article 클래스에 createDate, modifyDate 변수 추가

--- src/main/java/com/gompnag/tutorial1/boundedContext/article/entity/Article.java
+++ src/main/java/com/gompnag/tutorial1/boundedContext/article/entity/Article.java
@@ -1,12 +1,21 @@
 package com.gompnag.tutorial1.boundedContext.article.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+
+import java.time.LocalDateTime;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
 
 @Entity
 public class Article {
   @Id // primary key가 id 칼럼에 속성 적용
+  @GeneratedValue(strategy = IDENTITY)  // AUTO_INCREMENT
   private long id;
+  private LocalDateTime createDate;
+  private LocalDateTime modifyDate;
   private String subject;
   private String content;
 }
