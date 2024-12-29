8e36172daed7b37cc56021d25b1c0632e9e97038
Gompangii <gompangii@gmail.com>
2024-12-29 14:57:10 +0900

    52강, Article 클래스에 생성자 메서드 추가

--- src/main/java/com/gompnag/tutorial1/boundedContext/article/entity/Article.java
+++ src/main/java/com/gompnag/tutorial1/boundedContext/article/entity/Article.java
@@ -4,8 +4,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
@@ -14,6 +16,8 @@ import static jakarta.persistence.GenerationType.IDENTITY;
 @Entity
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Article {
   @Id // primary key가 id 칼럼에 속성 적용
   @GeneratedValue(strategy = IDENTITY)  // AUTO_INCREMENT
